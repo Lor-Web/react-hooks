b5b2fc9435c45be3222ea604b8da18dd1e0de840
Ruslan Ruya <lorelin97@yandex.ru>
2024-02-22 16:18:11 +0300

    set debounce input value

--- react-hooks-tutorial/src/App.tsx
+++ react-hooks-tutorial/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {UseEffectShowTextComponent} from "./components/useEffectShowText/useEffectShowText.tsx";
 import {useState} from "react";
+import {UseEffectTimeoutComponent} from "./components/useEffectTimeout/UseEffectTimeout.tsx";
 
 function App() {
     const [currentText, setText] = useState<string>('hello')
@@ -9,6 +10,9 @@ function App() {
     <>
       <UseEffectShowTextComponent text={currentText} />
         <button onClick={() => setText(() => 'time:' + new Date().getSeconds().toString())}>go new text</button>
+        <br/>
+        <br/>
+        <UseEffectTimeoutComponent></UseEffectTimeoutComponent>
     </>
   )
 }
---
+++ react-hooks-tutorial/src/components/useEffectTimeout/UseEffectTimeout.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from "react";
+
+export const UseEffectTimeoutComponent = () => {
+    const [inputValue, setInputValue] = useState<string>('');
+    const [value, setValue] = useState<string>('')
+
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {setValue(inputValue)}, 300)
+
+        console.log('value:', value, 'inputValue:', inputValue)
+
+        return () => {
+            clearTimeout(timeOutId)
+        }
+
+    }, [inputValue]);
+
+    const changeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log('change:', event.target.value)
+        setInputValue(event.target.value)
+    }
+
+    return (
+        <>
+            <input type="text"  onChange={changeValue}/>
+            <h2>Input Value (300ms): {value}</h2>
+        </>
+    )
+}
